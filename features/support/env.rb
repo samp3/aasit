54772e241e9b3ec578760cde4183959bdf5413a3
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-13 16:17:54 +0300

    coveralls

--- features/support/env.rb
+++ features/support/env.rb
@@ -10,11 +10,11 @@ require 'capybara/dsl'
 require 'capybara/cucumber'
 require 'capybara/rspec'
 require 'simplecov'
-
+require 'coveralls'
 include RSpec::Matchers
 
 SimpleCov.start
-
+Coveralls.wear!('rails')
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
 # selectors in your step definitions to use the XPath syntax.
