e1a2b47d8b448a44b37d9ecc815725b3e2990f32
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-13 15:42:36 +0300

    featuret

--- features/web/step_definitions/web_steps.rb
+++ features/web/step_definitions/web_steps.rb
@@ -1,8 +1,74 @@
-
+Given(/^Järjestelmässä on viite, jonka lyhytnimi on "([^"]*)" ja tyyppi "([^"]*)"$/)  do |slug,type|
+  Reftype.create!(name: type, id:1)
+  tyyppi = Reftype.find_by_name(type)
+  Ref.create!(slug: slug, reftype_id: tyyppi.id)
+  @artikkeli = Ref.find_by_slug(slug)
+end
 When(/^Käyttäjä siirtyy sivulle refs$/) do
-  visit '/refs.json'
-  page.should have_content("1")
+  visit refs_path
 end
 Then(/^hän näkee listan järjestelmässä olevista viitteistä$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.should have_content(@artikkeli.slug)
 end
+
+Given(/^Järjestelmässä on viitetyyppi "([^"]*)"$/) do |type|
+  Reftype.create!(name: type, id:1)
+end
+
+When(/^Käyttäjä siirtyy viitteen luontisivulle$/) do
+  visit new_ref_path
+end
+When(/^Luo artikkelin lyhytnimellä "([^"]*)"$/)  do |slug|
+  fill_in 'Slug', :with => slug
+  click_button('Create Ref')
+end
+Then(/^Käyttäjälle ilmoitetaan tapahtuman onnistumisesta ja ohjataan artikkelin sivulle$/) do
+  page.should have_content("Ref was successfully created.")
+end
+Then(/^Käyttäjälle ilmoitetaan tapahtuman epäonnistumisesta$/) do
+  page.should have_content("Slug can't be blank")
+end
+Given(/^Järjestelmässä on artikkeli lyhytnimellä "([^"]*)"$/) do |slug|
+  Ref.delete_all
+  Reftype.create!(name: "article", id:1)
+
+  Ref.create!(slug: slug, reftype_id: 1)
+end
+And(/^Painaa nappia "destroy"$/) do
+  click_link('Destroy')
+end
+Then(/^Kyseinen viite poistuu järjestelmästä$/) do
+  expect(page).not_to have_content("Slug can't be blank")
+end
+When(/^Käyttäjä painaa "([^"]*)"\-linkkiä, muokkaa lyhytnimeksi "([^"]*)" ja painaa painiketta Update Ref$/)  do |linkki, slug|
+  click_link(linkki)
+  fill_in 'Slug', :with => slug
+  click_button('Update Ref')
+end
+Then(/^Käyttäjälle ilmoitetaan päivityksen onnistumisesta$/) do
+  page.should have_content("Ref was successfully updated.")
+end
+Given(/^Järjestelmässä on viite, jonka lyhytnimi on "([^"]*)" ja tyyppi "([^"]*)" ja atrribuutti "([^"]*)"$/)  do |slug, type, attribute|
+  Ref.delete_all
+  Reftype.delete_all
+  RefAttribute.delete_all
+  Reftype.create!(name:type, id:1)
+  Ref.create!(slug:slug, reftype_id:1)
+  RefAttribute.create!(name:attribute, id:1)
+end
+Given(/^Käyttäjä on sivulla ref_meta$/) do
+  visit ref_meta_path
+end
+When(/^Käyttäjä painaa linkkiä "([^"]*)"$/) do |link|
+  click_link(link)
+end
+And(/^Käyttäjä syöttää kenttään Value "([^"]*)" ja painaa nappia "([^"]*)"$/) do |value,button|
+  fill_in 'Value', :with => value
+  click_button(button)
+end
+Then(/^Käyttäjälle ilmoitetaan onnistumisesta ja ohjataan viitteen sivulle$/) do
+  page.should have_content("Ref metum was successfully created.")
+end
+#Update Ref
+#Ref was successfully updated.
+#
--- features/web/web.feature
+++ features/web/web.feature
@@ -1,5 +1,33 @@
 Feature: Käyttäjänä pystyn käyttämään järjestelmää selaimella
 
   Scenario: Pystyn aukaisemaan viitelistan selaimella
+    Given Järjestelmässä on viite, jonka lyhytnimi on "ÄÖ01!" ja tyyppi "article"
     When Käyttäjä siirtyy sivulle refs
     Then hän näkee listan järjestelmässä olevista viitteistä
+
+  Scenario: Käyttäjä luo viitteen järjestelmään ja päivittää lyhytnimeä
+    Given Järjestelmässä on viitetyyppi "article"
+    When Käyttäjä siirtyy viitteen luontisivulle
+    And Luo artikkelin lyhytnimellä "ÄÖ!2"
+    Then Käyttäjälle ilmoitetaan tapahtuman onnistumisesta ja ohjataan artikkelin sivulle
+    When Käyttäjä painaa "Edit"-linkkiä, muokkaa lyhytnimeksi "ÖÖ10" ja painaa painiketta Update Ref
+    Then Käyttäjälle ilmoitetaan päivityksen onnistumisesta
+
+  Scenario: Käyttäjä luo nimettömän viitteen järjestelmään
+    Given Järjestelmässä on viitetyyppi "article"
+    When Käyttäjä siirtyy viitteen luontisivulle
+    And Luo artikkelin lyhytnimellä ""
+    Then Käyttäjälle ilmoitetaan tapahtuman epäonnistumisesta
+
+  Scenario: Käyttäjä luo nimettömän viitteen järjestelmään
+    Given Järjestelmässä on artikkeli lyhytnimellä "POISTA00"
+    When Käyttäjä siirtyy sivulle refs
+    And Painaa nappia "destroy"
+    Then Kyseinen viite poistuu järjestelmästä
+
+    Scenario: Käyttäjä pystyy lisämään attribuutin viitteelle
+      Given Järjestelmässä on viite, jonka lyhytnimi on "ÄÖ01!" ja tyyppi "article" ja atrribuutti "author"
+      Given Käyttäjä on sivulla ref_meta
+      When Käyttäjä painaa linkkiä "New Ref metum"
+      And Käyttäjä syöttää kenttään Value "testaaja" ja painaa nappia "Create Ref metum"
+      Then Käyttäjälle ilmoitetaan onnistumisesta ja ohjataan viitteen sivulle
