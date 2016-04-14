963b827da5e5c5ff1fcd155abac053963aae4029
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-14 15:23:23 +0300

    aloitussivuksi refs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :ref_type_fields
   resources :ref_meta
   resources :ref_attributes
   resources :refs
@@ -7,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'refs#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
