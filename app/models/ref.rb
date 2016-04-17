5ae478b10a85602a3984dac93712410d7527629a
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-17 21:15:05 +0300

    apumetodi attribuuttien arvojen hakemiseen

--- app/models/ref.rb
+++ app/models/ref.rb
@@ -1,7 +1,6 @@
 class Ref < ActiveRecord::Base
-   belongs_to :reftype
-   has_many :ref_metum
-
+  belongs_to :reftype
+  has_many :ref_metum
 
   validates :reftype_id, presence: true
   validates :slug , presence: true
@@ -9,5 +8,8 @@ class Ref < ActiveRecord::Base
   def requiredFields?
     requiredIds = reftype.requiredFields.ids - self.ref_metum.pluck(:ref_attribute_id)
     requiredIds.length.zero?
+  end
+  def fields
+    Hash[ref_metum.joins(:ref_attribute).pluck(:name, :value)]
   end
 end
