8065ea3e45f9493b40a42e8ca833fa6023f2f423
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-19 14:15:37 +0300

    päivitetty seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,51 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Määritellään tallennettavat attribuutit (viitteen kentät)
 fields = ['address','annote','author','booktitle','chapter','crossref','edition','editor','howpublished','institution',
           'journal','key','month','note','number','organization','pages','publisher','school','series','title','type',
           'volume','year']
+#Määritellään tallennettavat tyypit
 types = ['article','book','booklet','conference','inbook','incollection','inproceedings','manual','mastersthesis',
          'misc','phdthesis','proceedings','techreport','unpublished']
-
+#Määritellään tyypeittäin pakolliset ja valinnaiset kentät
 reftypefields = Hash.new
-reftypefields["article"] = {"Required":['author','title','journal','year','volume'],
-                            "Optional":['number','pages','month','note','key']}
+reftypefields['article'] = {required:['author','title','journal','year','volume'],
+                            optional:['number','pages','month','note','key']}
 
+
+#Luodaan attribuutit
 fields.each do |field|
-  f = RefAttribute.new(name:field)
-  f.save
+  f = RefAttribute.create!(name:field)
 end
+#Luodaan tyypi
 types.each do |type|
-  f = Reftype.new(name:type)
-  f.save
+  f = Reftype.create!(name:type)
 end
-ref = Ref.create!(slug:'W04', reftype_id:1)
+ref = Ref.create!(slug:'W04', reftype_id:Reftype.find_by_name('article').id)
+#Luodaan esimerkkiviite
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('author').id, value: 'Whittington, Keith J.')
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('title').id, value: 'Infusing active learning into introductory programming courses')
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('journal').id, value: 'J. Comput. Small Coll.')
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('volume').id, value: '19')
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('number').id, value: '5')
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('year').id, value: '2004')
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('pages').id, value: '249--259')
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('publisher').id, value: 'Consortium for Computing Sciences in Colleges')
+RefMetum.create!(ref_id:ref.id,ref_attribute_id:RefAttribute.find_by_name('address').id, value: 'USA')
 
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:3, value: "Whittington, Keith J.")
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:21, value: "Infusing active learning into introductory programming courses")
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:11, value: "J. Comput. Small Coll.")
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:23, value: "19")
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:15, value: "5")
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:24, value: "2004")
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:17, value: "249--259")
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:18, value: "Consortium for Computing Sciences in Colleges")
-RefMetum.create!(ref_id:ref.id,ref_attribute_id:1, value: "USA")
-
+#Tallennetaan tieto pakollisista ja valinnaisista kentistä
 reftypefields.keys.each do |type|
 
   reftypefields[type].keys.each do |h|
     reftypefields[type][h].each do |field|
-       t = Reftype.find_by_name(type)
-       f = RefAttribute.find_by_name(field)
+      ref_attribute_id = Reftype.find_by_name(type).id
+      reftype_id= RefAttribute.find_by_name(field).id
 
-       if h == :Required
-         RefTypeField.create!(ref_attribute_id: f.id, reftype_id: t.id, obligatory: 1)
-       elsif h == :Optional
-         RefTypeField.create!(ref_attribute_id: f.id, reftype_id: t.id, obligatory: 0)
+       if h == :required
+         RefTypeField.create!(ref_attribute_id: ref_attribute_id, reftype_id: reftype_id, obligatory: 1)
+       elsif h == :optional
+         RefTypeField.create!(ref_attribute_id: ref_attribute_id, reftype_id: reftype_id, obligatory: 0)
        end
      end
   end
