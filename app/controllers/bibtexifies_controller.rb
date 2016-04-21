304a5410adf6012b519f51763f1b0090fecf584a
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-21 11:00:49 +0300

    siistiminen

--- app/controllers/bibtexifies_controller.rb
+++ app/controllers/bibtexifies_controller.rb
@@ -1,11 +1,14 @@
 class BibtexifiesController < ApplicationController
   def download
-     @refs = []
-     Ref.all.each do |ref|
-       if ref.requiredFields?
-         @refs.append(ref)
-       end
-     end
+     #@refs = []
+     #Ref.all.each do |ref|
+     #  if ref.requiredFields?
+     #    @refs.append(ref)
+     #  end
+     #end
+    #Otetaan vain ne viitteet joiden pakolliset kentät on asetettu
+    @refs = Ref.all.select {|ref| ref.requiredFields?}
+
     response.headers["Content-Type"] = 'application/x-bibtex'
     response.headers["Content-Disposition"] = ' attachment; filename=sigproc.bib'
     render 'bibtex',:layout => false
