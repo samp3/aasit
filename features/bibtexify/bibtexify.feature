352f109e3986327ffabd13c84ee63a82310a6ebd
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-21 11:32:17 +0300

    uusia hienoja testejä

---
+++ features/bibtexify/bibtexify.feature
@@ -0,0 +1,11 @@
+Feature: Käyttäjänä haluan pystyä generoimaan bibtex-tiedoston järjestelmässä olevista viitteistä
+
+  Scenario: Käyttäjä painaa Lataa BibTeX-tiedosto -linkkiä
+    Given Järjestelmässä on viite, jonka lyhytnimi on "ÄÖ01!", tyyppi "article". Viitteen pakolliset tiedot "on" täytetty.
+    When Käyttäjä menee viitteiden sivulle ja painaa linkkiä "Lataa BibTeX-tiedosto"
+    Then Järjestelmä palauttaa käyttäjälle BibTeX-tiedoston
+
+  Scenario: Merkkijonon muodostus
+    Given Järjestelmässä on viite, jonka lyhytnimi on "ÄÖ01!", tyyppi "article". Viitteen pakolliset tiedot "on" täytetty.
+    When RefMetum osaa antaa rivin merkkijonona
+    Then Ref osaa antaa viitteen tiedot merkkijonona
---
+++ features/bibtexify/step_definitions/bibtexify_steps.rb
@@ -0,0 +1,76 @@
+
+Given(/^Järjestelmässä on viite, jonka lyhytnimi on "([^"]*)", tyyppi "([^"]*)"\. Viitteen pakolliset tiedot "([^"]*)" täytetty\.$/) do |slug,type,ok|
+
+
+
+  tiedotOk = ok == "on"
+  @reftype = Reftype.find_by_name(type)
+  @ref = Ref.find_by_slug(slug)
+  @required = ['author','title','journal','year','volume']
+  @optional = ['number','pages','month','note','key']
+  #Tyyppi
+  unless @reftype
+    createReftype(type)
+    @reftype = Reftype.find_by_name(type)
+  end
+  #Tyypin kentät ja pakolliset kentät
+    @required.each do |field|
+      processRefTypeField(field,type,1)
+    end
+  #Vapaavalintaiset kentät
+    @optional.each do |field|
+      processRefTypeField(field,type,0)
+    end
+  #Viite
+  unless @ref
+    create_ref_with_expect(slug, type)
+    @ref = Ref.find_by_slug(slug)
+  end
+  #Rivejä
+  @required.each do |ref_attribute|
+    randValue = (0...50).map { ('a'..'z').to_a[rand(26)] }.join
+
+    createRefMetumWithExpect(@ref.slug, ref_attribute, randValue)
+  end
+  unless tiedotOk
+    @ref.ref_metum.last.delete
+  end
+end
+When(/^Käyttäjä menee viitteiden sivulle ja painaa linkkiä "([^"]*)"$/) do |link|
+  visit '/refs'
+  click_link(link)
+end
+Then(/^Järjestelmä palauttaa käyttäjälle BibTeX-tiedoston$/) do
+  page.response_headers['Content-Disposition'].should have_content("attachment")
+  page.response_headers['Content-Type'].should  eq('application/x-bibtex')
+end
+When(/^RefMetum osaa antaa rivin merkkijonona$/) do
+  @bib = Hash.new
+  ref_meta = Ref.first.ref_metum
+  ref_meta.each do |ref_metum|
+    attribute, value  = /([^"]*) = { ([^"]*) }/.match(ref_metum.to_s).captures
+    expect(attribute).to eq(RefAttribute.find(ref_metum.ref_attribute_id).name)
+    expect(value).to eq(ref_metum.value)
+    @bib[attribute] = value
+  end
+end
+Then(/^Ref osaa antaa viitteen tiedot merkkijonona$/) do
+  i =1
+  @ref = Ref.first
+  type = @ref.reftype.name
+  @ref.to_s.each_line do |line|
+    if i == 1
+      r_type, r_slug = /@([^"]*)\{([^"]*)\,$/.match(line).captures
+      expect(r_type).to eq(type)
+      expect(r_slug).to eq(@ref.slug)
+    elsif i > (@ref.ref_metum.length + 1)
+      puts i.to_s + " <> " + @ref.ref_metum.length.to_s + " " + line
+    else
+      attribute, value  = /([^"]*) = { ([^"]*) }/.match(line).captures
+      expect(value).to eq(@bib[attribute])
+      @bib.delete(attribute)
+    end
+    i += 1
+  end
+end
+
