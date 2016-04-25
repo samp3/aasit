c5a4401f90836475c71fe544c231dfc62a530b3f
Arto Rantanen <arto.rantanen@helsinki.fi>
2016-04-25 14:14:38 +0300

    web testausta

--- features/web/step_definitions/web_steps.rb
+++ features/web/step_definitions/web_steps.rb
@@ -1,3 +1,54 @@
+Given(/^Lue seeds\.rb$/) do
+  Ref.delete_all
+  Reftype.delete_all
+  RefAttribute.delete_all
+  RefMetum.delete_all
+  RefTypeField.delete_all
+#Määritellään tallennettavat attribuutit (viitteen kentät)
+  fields = ['address','annote','author','booktitle','chapter','crossref','edition','editor','howpublished','institution',
+            'journal','key','month','note','number','organization','pages','publisher','school','series','title','type',
+            'volume','year']
+#Määritellään tallennettavat tyypit
+  types = ['article','book','booklet','conference','inbook','incollection','inproceedings','manual','mastersthesis',
+           'misc','phdthesis','proceedings','techreport','unpublished']
+#Määritellään tyypeittäin pakolliset ja valinnaiset kentät
+  reftypefields = Hash.new
+  reftypefields['article'] = {required:['author','title','journal','year','volume'],
+                              optional:['number','pages','month','note','key']}
+  reftypefields['book'] = {   required:['author','editor','title','publisher','year'],
+                              optional:['volume','number','series','address','month','note','key']}
+  reftypefields['inproceedings'] = {required:['author','title','booktitle','year'],
+                                    optional:['editor','volume','number','series','pages','address','month','organization','publisher','note','key']}
+
+
+#Luodaan attribuutit
+  fields.each do |field|
+    f = RefAttribute.create!(name:field)
+  end
+#Luodaan tyypi
+  types.each do |type|
+    f = Reftype.create!(name:type)
+  end
+
+#Tallennetaan tieto pakollisista ja valinnaisista kentistä
+  reftypefields.keys.each do |type|
+
+    reftypefields[type].keys.each do |h|
+      reftypefields[type][h].each do |field|
+        ref_attribute_id =RefAttribute.find_by_name(field).id
+        reftype_id= Reftype.find_by_name(type).id
+
+        if h == :required
+          RefTypeField.create!(ref_attribute_id: ref_attribute_id, reftype_id: reftype_id, obligatory: 1)
+        elsif h == :optional
+          RefTypeField.create!(ref_attribute_id: ref_attribute_id, reftype_id: reftype_id, obligatory: 0)
+        end
+      end
+    end
+
+  end
+
+end
 Given(/^Järjestelmässä on viite, jonka lyhytnimi on "([^"]*)" ja tyyppi "([^"]*)"$/)  do |slug,type|
   Reftype.create!(name: type, id:1)
   tyyppi = Reftype.find_by_name(type)
@@ -16,11 +67,11 @@ Given(/^Järjestelmässä on viitetyyppi "([^"]*)"$/) do |type|
 end
 
 When(/^Käyttäjä siirtyy viitteen luontisivulle$/) do
-  visit new_ref_path
+  visit 'refs/new'
 end
 When(/^Luo artikkelin lyhytnimellä "([^"]*)"$/)  do |slug|
-  fill_in 'Slug', :with => slug
-  click_button('Create Ref')
+  fill_in 'ref[slug]', :with => slug
+  click_button('Anna viitteelle tunniste ja tyyppi')
 end
 Then(/^Käyttäjälle ilmoitetaan tapahtuman onnistumisesta ja ohjataan artikkelin sivulle$/) do
   pending
@@ -31,10 +82,13 @@ Then(/^Käyttäjälle ilmoitetaan tapahtuman epäonnistumisesta$/) do
   pending
 end
 Given(/^Järjestelmässä on artikkeli lyhytnimellä "([^"]*)"$/) do |slug|
-  Ref.delete_all
-  Reftype.create!(name: "article", id:1)
+  article = Reftype.find_by_name('article')
+  unless article
+    Reftype.create!(name: "article", id:1)
+  end
 
   Ref.create!(slug: slug, reftype_id: 1)
+  @artikkeli = Ref.find_by_slug(slug)
 end
 And(/^Painaa nappia "destroy"$/) do
 
@@ -61,6 +115,9 @@ Given(/^Järjestelmässä on viite, jonka lyhytnimi on "([^"]*)" ja tyyppi "([^"
   Ref.create!(slug:slug, reftype_id:1)
   RefAttribute.create!(name:attribute, id:1)
 end
+Given(/^Järjestelmässä on viite, jonka lyhytnimi on "([^"]*)"$/)  do |slug|
+  Ref.create!(slug:slug, reftype_id:Reftype.find_by_name('article'))
+end
 Given(/^Käyttäjä on sivulla ref_meta$/) do
   visit ref_meta_path
 end
@@ -72,7 +129,7 @@ And(/^Käyttäjä syöttää kenttään Value "([^"]*)" ja painaa nappia "([^"]*
   click_button(button)
 end
 Then(/^Käyttäjälle ilmoitetaan onnistumisesta ja ohjataan viitteen sivulle$/) do
-  page.should have_content("Ref metum was successfully created.")
+  page.should have_content("Tuntamaton lyhytnimi")
 end
 #Update Ref
 #Ref was successfully updated.
--- features/web/web.feature
+++ features/web/web.feature
@@ -1,28 +1,31 @@
 Feature: Käyttäjänä pystyn käyttämään järjestelmää selaimella
 
   Scenario: Pystyn aukaisemaan viitelistan selaimella
-    Given Järjestelmässä on viite, jonka lyhytnimi on "ÄÖ01!" ja tyyppi "article"
+    Given Lue seeds.rb
+    Given Järjestelmässä on artikkeli lyhytnimellä "TESTI1"
     When Käyttäjä siirtyy sivulle refs
     Then hän näkee listan järjestelmässä olevista viitteistä
 
   Scenario: Käyttäjä luo viitteen järjestelmään ja päivittää lyhytnimeä
-    Given Järjestelmässä on viitetyyppi "article"
+    Given Lue seeds.rb
     When Käyttäjä siirtyy viitteen luontisivulle
     And Luo artikkelin lyhytnimellä "ÄÖ!2"
 
   Scenario: Käyttäjä luo nimettömän viitteen järjestelmään
-    Given Järjestelmässä on viitetyyppi "article"
+    Given Lue seeds.rb
     When Käyttäjä siirtyy viitteen luontisivulle
     And Luo artikkelin lyhytnimellä ""
 
   Scenario: Käyttäjä luo nimettömän viitteen järjestelmään
+    Given Lue seeds.rb
     Given Järjestelmässä on artikkeli lyhytnimellä "POISTA00"
     When Käyttäjä siirtyy sivulle refs
     And Painaa nappia "destroy"
     Then Kyseinen viite poistuu järjestelmästä
 
     Scenario: Käyttäjä pystyy lisämään attribuutin viitteelle
-      Given Järjestelmässä on viite, jonka lyhytnimi on "ÄÖ01!" ja tyyppi "article" ja atrribuutti "author"
+      Given Lue seeds.rb
+      Given Järjestelmässä on artikkeli lyhytnimellä "ÖLÖL12"
       Given Käyttäjä on sivulla ref_meta
       When Käyttäjä painaa linkkiä "New Ref metum"
       And Käyttäjä syöttää kenttään Value "testaaja" ja painaa nappia "Create Ref metum"
